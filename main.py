f7da53d98b1f0ec58c8909ffd718c8d1eefcdcf1
CihanNscc <alcincihan@gmail.com>
2024-08-28 21:11:58 -0300

    Basic syntax added

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
 # Phython Cheatsheet
 # ==================
+# ==================
+
+
+# Basic Syntax
+# ============
+# Comment
+# print("Hello, World!")
+
+# Variable
+a = 1
+
+
+# Function
+def add(a, b):
+    return a + b
+
+
+# Class
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+
+# Loop
+for i in range(10):
+    print(i)
+
+# Conditional If-Else
+if a == 1:
+    print("a is 1")
+elif a == 2:
+    print("a is 2")
+else:
+    print("a is neither 1 nor 2")
+
+# Conditional Switch
+match a:
+    case 1:
+        print("a is 1")
+    case 2:
+        print("a is 2")
+    case _:
+        print("a is neither 1 nor 2")
+
+# Conditional Ternary
+a = 1 if a == 1 else 0
+
 
 # Immutable Types
 # ===============
@@ -15,7 +66,7 @@ b = a  # "b" is assigned the value of "a"
 b = 3
 
 # "a" is still 2, because "b" is a separate variable
-print("a = ", a)
+print("a = ", a)  # a = 2
 
 
 # Mutable Types
@@ -28,6 +79,5 @@ num_list = [1, 2, 3]
 second_num_list = num_list  # "second_num_list" references the same list as "num_list"
 second_num_list.append(4)
 
-print(
-    "num_list = ", num_list
-)  # "num_list" is now [1, 2, 3, 4] because "second_num_list" is a reference
+# "num_list" is now [1, 2, 3, 4] because "second_num_list" is a reference
+print("num_list = ", num_list)  # num_list = [1, 2, 3, 4]
